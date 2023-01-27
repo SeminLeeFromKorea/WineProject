941aba9f170c5ae78180911f47edaa16dfaa9170
Semin Lee <leesemeen@naver.com>
2023-01-27 20:40:58 +0900

    20230127헤더수정

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -15,16 +15,26 @@ const Header = () => {
         nav(-1); // 뒤로가기, +1은 앞으로 가기
     }
 
-    const [leftSelect, setLeftSelect] = useState(false);
-    const [rightSelect, setRightSelect] = useState(false);
+    const [oneSelect, setOneSelect] = useState(false);
+    const [twoSelect, setTwoSelect] = useState(false);
+    const [threeSelect, setThreeSelect] = useState(false);
+    const [fourSelect, setFourSelect] = useState(false);
 
 
-    const handleLeftChild = () => {
-        setLeftSelect(!leftSelect);
+    const handleOneChild = () => {
+        setOneSelect(!oneSelect);
     }
 
-    const handleRightChild = () => {
-        setRightSelect(!rightSelect);
+    const handleTwoChild = () => {
+        setTwoSelect(!twoSelect);
+    }
+
+    const handleThreeChild = () => {
+        setThreeSelect(!threeSelect);
+    }
+
+    const handleFourChild = () => {
+        setFourSelect(!fourSelect);
     }
     const [st , setSt] =useState({display:"none"});
     const handleOn=()=>{
@@ -40,13 +50,21 @@ const Header = () => {
             <header className={styled.HeaderBySemin}>
                 <div className={styled.HeaderBySemin_left}>
                     <button className={styled.HeaderBySemin_leftchildbtn}><Link to ="/" style={{color:'white'}}><img style={{ width: '60px' }} src={'/wine.png'}></img></Link></button>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleLeftChild}><p>레드와인</p>
-                        <li className={!leftSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to ='/red' style={{color:'white'}}><span>목록보기</span></Link></li>
-                        <li className={!leftSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleOneChild}><p>레드와인</p>
+                        <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to ='/red' style={{color:'white'}}><span>목록보기</span></Link></li>
+                        <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
                     </ul>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleRightChild}><p>화이트와인</p>
-                        <li className={!rightSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to ='/white' style={{color:'white'}}><span>목록보기</span></Link></li>
-                        <li className={!rightSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleTwoChild}><p>화이트와인</p>
+                        <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to ='/white' style={{color:'white'}}><span>목록보기</span></Link></li>
+                        <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                    </ul>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleThreeChild}><p>스파클링</p>
+                        <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to ='/red' style={{color:'white'}}><span>목록보기</span></Link></li>
+                        <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                    </ul>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleFourChild}><p>로제</p>
+                        <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to ='/red' style={{color:'white'}}><span>목록보기</span></Link></li>
+                        <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
                     </ul>
                     <div className={`${styled.search}`}>
                         <input className={`${styled.searchInput}`} placeholder="검색어를 입력해주세요" />
