a52264c44f3a716ab33e6f1c75022020702613aa
jiwon <leejw0199@naver.com>
2023-01-27 18:03:10 +0900

    20230127 18:02 [UPDATE] 화이트와인 페이지네이션 및 상세페이지 연결

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './layout/Home';
 import Layout from './layout/Layout';
+import WhiteWineDetail from './JW/WhiteWineDetail';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route element={<Layout/>}>
           <Route path='/' element={<Home/>}/>
+          <Route path='/detail' element={<WhiteWineDetail/>}/>
         </Route>
       </Routes>
       
--- src/JW/WhiteWineAPI.js
+++ src/JW/WhiteWineAPI.js
@@ -1,55 +1,69 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { PuffLoader } from 'react-spinners';
 import WhiteWineList from './WhiteWhineList';
+import WhiteWinePagination from './WhiteWinePagination';
 
-import styled from './WhiteWineList.module.css';
-
+/* API 가져오기 */
 const WhiteWineAPI = () => {
+  
+  /** 로딩중일 때 */
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1); /* 데이터 1부터 보여줌 */
+  const [dataPerPage, setDatasPerPage] = useState(12); /* 12개씩 보여줄 데이터 관리 */
 
-    const [data, setData] = useState("");
     const getData = async () => {
       const resp = await fetch('https://api.sampleapis.com/wines/whites');
       const json = await resp.json();
-      console.log(json);
-      setData(json);
+      // console.log(json);
+      setData(json.filter( data => data.id !== 18 && data.id !== 42 )
+                  .filter( (data, index) => index < 60 ));
       setLoading(true); //로딩완료
     }
-  
+    
     useEffect(() => {
       getData();
     }, []);
-  
-    const [loading, setLoading] = useState(false);
-    if (loading === false) {
-      return <div className={styled.loading}>
-                <PuffLoader
-                  color="#36d7b7"
-                  cssOverride={{}}
-                  loading
-                  size={100}
-                  speedMultiplier={1}
-                />
-              </div>
-    }
+    
+    if(loading === false){ /* 로딩 마크 */
+    return <div>
+      <PuffLoader
+      color="#36d7b7"
+      cssOverride={{}}
+      loading
+      size={100}
+      speedMultiplier={1}
+      />
+    </div>
+  } 
 
+    /* pagination */
+
+    const indexOfLast = currentPage * dataPerPage; /* 마지막 인덱스 값 */
+    const indexOfFirst = indexOfLast - dataPerPage;/* 처음 인덱스 값 */
+
+    const currentDatas = (data) => {
+      let currentDatas = 0;
+      currentDatas = data.slice(indexOfFirst, indexOfLast);
+      return currentDatas;
+    }
+    
     return (
-      
-      <div className={styled.whitewine_container}>
-        <h3> 화이트 와인 관련 이미지 </h3>
-        <ul className={styled.whitewine_wrap}>
-          {
-            data.map( function(item) {
-              if(item.id !== 18 && item.id !== 42 && item.id !== 85 && item.id !== 154 && item.id !== 169 && item.id !== 186 && item.id !== 193 &&
-                 item.id !== 204 && item.id !== 208 && item.id !== 215 && item.id !== 219 && item.id !== 237 && item.id !== 248 && item.id !== 250 &&
-                 item.id !== 264 && item.id !== 265 && item.id !== 328 && item.id !== 333 && item.id !== 340 && item.id !== 379 && item.id !== 394 &&
-                 item.id !== 441 && item.id !== 442) {
-                return <WhiteWineList key={item.id} item={item}/> 
-              }
-            } )
-          }
-        </ul>
+      <div>
+        
+          {/* <WhiteWineList data={data} /> */}
+        <WhiteWineList data={currentDatas(data)} />
+
+        <WhiteWinePagination
+          postsPerPage={dataPerPage}
+          totalPosts={data.length}
+          paginate={setCurrentPage}
+        ></WhiteWinePagination>
+
       </div>
+
+
     )
-}
+      }
 
 export default WhiteWineAPI;
