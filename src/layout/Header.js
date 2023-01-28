901743ef6f4ba4b2daf35e462a51b5254e7c1caa
Semin Lee <leesemeen@naver.com>
2023-01-28 15:28:59 +0900

    20230128semin

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -48,17 +48,7 @@ const Header = () => {
         }
     }
 
-
-
-
     const [st, setSt] = useState({ display: "none" });
-    const handleOn = () => {
-        setSt({ display: "block" });
-    }
-    const handleOn2 = () => {
-        setSt({ display: "none" });
-
-    }
 
     const goToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth"});
@@ -69,19 +59,19 @@ const Header = () => {
             <header className={styled.HeaderBySemin}>
                 <div className={styled.HeaderBySemin_left}>
                     <button className={styled.HeaderBySemin_leftchildbtn}><Link to="/" style={{ color: 'white' }}><img style={{ width: '60px' }} src={'/wine.png'}></img></Link></button>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p>레드와인</p>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>레드와인</p>
                         <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
                         <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
                     </ul>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p>화이트와인</p>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>화이트와인</p>
                         <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/white' style={{ color: 'white' }}><span>목록보기</span></Link></li>
                         <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
                     </ul>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p>스파클링</p>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>스파클링</p>
                         <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
                         <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
                     </ul>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p>로제</p>
+                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>로제</p>
                         <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
                         <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
                     </ul>
@@ -90,13 +80,13 @@ const Header = () => {
                         <button className={`${styled.searchBtn}`}><img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAzNiAzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZmlsbD0ibm9uZSIgZD0iTTAgMGgzNnYzNkgweiIvPgogICAgICAgIDxnIHN0cm9rZT0iIzVGMDA4MCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2Utd2lkdGg9IjEuNyI+CiAgICAgICAgICAgIDxwYXRoIGQ9Im0yNi4wODEgMjYuMDgxLTQuMTItNC4xMk0xNi40NjcgMjMuMzM0YTYuODY3IDYuODY3IDAgMSAwIDAtMTMuNzM0IDYuODY3IDYuODY3IDAgMCAwIDAgMTMuNzM0eiIvPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+Cg=="></img></button>
                     </div>
                 </div>
-                <div className={styled.HeaderBySemin_left}>
+                {/* <div className={styled.HeaderBySemin_left}>
                     <p className={styled.category} onClick={handleOn} >三 카테고리</p>
 
-                </div>
+                </div> */}
 
             </header>
-            <div className={styled.hiddenBox}>
+            {/* <div className={styled.hiddenBox}>
                 <ul className={styled.hiddenCategory} style={st} onMouseOver={handleOn} onMouseOut={handleOn2}>
                     <li><Link to="/" style={{ color: 'white' }}>HOME</Link></li>
                     <li><Link to='/red' style={{ color: 'white' }}>RedWine</Link></li>
@@ -104,10 +94,10 @@ const Header = () => {
                     <li>Sparkling</li>
                     <li>Rose</li>
                 </ul>
-            </div>
+            </div> */}
 
             <div className={styled.topbutton} onClick={goToTop}>
-                <span>TOP</span>
+                <span style={{fontFamily: 'Nanum Gothic'}}>TOP</span>
             </div>
 
             <section>
