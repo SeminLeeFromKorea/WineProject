eb922ff8e458cde5b24f33092b29654284af9286
Semin Lee <leesemeen@naver.com>
2023-01-28 16:01:47 +0900

    gd

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -51,30 +51,32 @@ const Header = () => {
     const [st, setSt] = useState({ display: "none" });
 
     const goToTop = () => {
-        window.scrollTo({ top: 0, behavior: "smooth"});
+        window.scrollTo({ top: 0, behavior: "smooth" });
     };
 
     return (
         <>
             <header className={styled.HeaderBySemin}>
-                <div className={styled.HeaderBySemin_left}>
-                    <button className={styled.HeaderBySemin_leftchildbtn}><Link to="/" style={{ color: 'white' }}><img style={{ width: '60px' }} src={'/wine.png'}></img></Link></button>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>레드와인</p>
-                        <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
-                        <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
-                    </ul>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>화이트와인</p>
-                        <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/white' style={{ color: 'white' }}><span>목록보기</span></Link></li>
-                        <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
-                    </ul>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>스파클링</p>
-                        <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
-                        <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
-                    </ul>
-                    <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{fontFamily: 'Nanum Gothic'}}>로제</p>
-                        <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
-                        <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
-                    </ul>
+                <div className={styled.fixedHeader}>
+                    <div className={styled.HeaderBySemin_left}>
+                        <button className={styled.HeaderBySemin_leftchildbtn}><Link to="/" style={{ color: 'white' }}><img style={{ width: '60px' }} src={'/wine.png'}></img></Link></button>
+                        <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{ fontFamily: 'Nanum Gothic' }}>레드와인</p>
+                            <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
+                            <li className={!oneSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                        </ul>
+                        <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{ fontFamily: 'Nanum Gothic' }}>화이트와인</p>
+                            <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/white' style={{ color: 'white' }}><span>목록보기</span></Link></li>
+                            <li className={!twoSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                        </ul>
+                        <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{ fontFamily: 'Nanum Gothic' }}>스파클링</p>
+                            <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
+                            <li className={!threeSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                        </ul>
+                        <ul className={styled.HeaderBySemin_leftchild} onClick={handleChild}><p style={{ fontFamily: 'Nanum Gothic' }}>로제</p>
+                            <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><Link to='/red' style={{ color: 'white' }}><span>목록보기</span></Link></li>
+                            <li className={!fourSelect ? styled.HeaderBySemin_lefthiddenchild_default : styled.HeaderBySemin_lefthiddenchild_clicked}><span>안주추천</span></li>
+                        </ul>
+                    </div>
                     <div className={`${styled.search}`}>
                         <input className={`${styled.searchInput}`} placeholder="검색어를 입력해주세요" />
                         <button className={`${styled.searchBtn}`}><img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzYiIGhlaWdodD0iMzYiIHZpZXdCb3g9IjAgMCAzNiAzNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZmlsbD0ibm9uZSIgZD0iTTAgMGgzNnYzNkgweiIvPgogICAgICAgIDxnIHN0cm9rZT0iIzVGMDA4MCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2Utd2lkdGg9IjEuNyI+CiAgICAgICAgICAgIDxwYXRoIGQ9Im0yNi4wODEgMjYuMDgxLTQuMTItNC4xMk0xNi40NjcgMjMuMzM0YTYuODY3IDYuODY3IDAgMSAwIDAtMTMuNzM0IDYuODY3IDYuODY3IDAgMCAwIDAgMTMuNzM0eiIvPgogICAgICAgIDwvZz4KICAgIDwvZz4KPC9zdmc+Cg=="></img></button>
@@ -97,7 +99,7 @@ const Header = () => {
             </div> */}
 
             <div className={styled.topbutton} onClick={goToTop}>
-                <span style={{fontFamily: 'Nanum Gothic'}}>TOP</span>
+                <span style={{ fontFamily: 'Nanum Gothic' }}>TOP</span>
             </div>
 
             <section>
